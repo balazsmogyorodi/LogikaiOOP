8fc68124a3e44a3693f76c3d3eee374f2ab0612e
Venc03 <vegi.dani1@gmail.com>
2024-01-08 15:14:23 +0100

    mergelesek

--- src/logikaioop_csoport/LogikaiOOP_csoport.java
+++ src/logikaioop_csoport/LogikaiOOP_csoport.java
@@ -4,21 +4,9 @@ import java.util.Scanner;
 
 public class LogikaiOOP_csoport {
     
-<<<<<<< HEAD
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
         Lada a = new Lada();
-=======
-<<<<<<< HEAD
-    
-    
-   
-    public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);  
-=======
-    public static void main(String[] args) {
-        Lada a = new Lada();
->>>>>>> Dani
->>>>>>> b77170e6ca83f29ab744999cd0e023884d244297
         System.out.println("Leírás");
         System.out.println(a);
         System.out.println("Melyik:");
