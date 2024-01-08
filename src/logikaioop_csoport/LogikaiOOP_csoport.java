e4e74f679949bff9d14662fe90d601879b899f8e
Mogyoródi Balázs <mogyorodi.balazs@dk.akkszalezi.hu>
2024-01-08 15:04:05 +0100

    merge

--- src/logikaioop_csoport/LogikaiOOP_csoport.java
+++ src/logikaioop_csoport/LogikaiOOP_csoport.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class LogikaiOOP_csoport {
     
+<<<<<<< HEAD
     
     
    
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);  
+=======
+    public static void main(String[] args) {
+        Allitasok a = new Allitasok();
+>>>>>>> fejlesztoi
         System.out.println("Leírás");
-        System.out.println("Az 1. állítás!");
-        System.out.println("A 2. állítás!");
-        System.out.println("A 3. állítás!");
+        System.out.println(a);
         System.out.println("Melyik:");
         System.out.println("A válasz jo | rossz");
         String username = sc.nextLine();
