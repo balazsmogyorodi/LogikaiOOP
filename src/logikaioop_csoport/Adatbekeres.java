258885a0b1512e70ee6e6131bdf1809f2303d278
mogyorodi.balazs <mogyorodi.balazs@FUJI106win10.szaleziakk.local>
2024-01-08 15:45:18 +0100

    kiválasztás tétel

---
+++ src/logikaioop_csoport/Adatbekeres.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logikaioop_csoport;
+
+import java.util.ArrayList;
+
+
+
+public class Adatbekeres {
+    private String leiras;
+    private int melyik;
+    
+    
+    
+    public void run(){
+        
+       
+            
+            
+        
+    
+    
+    }
+    
+    public boolean isHelyes(){
+        if (melyik == 1){
+        return true;   
+        } else{
+        return false;
+        }
+    }
+    
+    public void eredmenyKonzolra(){
+    
+    
+    
+    }
+    
+}
--- src/logikaioop_csoport/LogikaiOOP_csoport.java
+++ src/logikaioop_csoport/LogikaiOOP_csoport.java
@@ -5,14 +5,7 @@ import java.util.Scanner;
 public class LogikaiOOP_csoport {
    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  
-        Lada a = new Lada();
-        System.out.println("Leírás");
-        System.out.println(a);
-        System.out.println("Melyik:");
-        System.out.println("A válasz jo | rossz");
-        String username = sc.nextLine();
-        System.out.println(username);
+        new Adatbekeres().run();
     }
     
 }
