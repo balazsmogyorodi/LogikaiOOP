927f019a027303aacda681100dae10fa0829f9ba
Venc03 <vegi.dani1@gmail.com>
2024-01-08 15:53:52 +0100

    ora vege, feladat class krealas, main 1 sor

--- src/logikaioop_csoport/LogikaiOOP_csoport.java
+++ src/logikaioop_csoport/LogikaiOOP_csoport.java
@@ -1,13 +1,9 @@
 package logikaioop_csoport;
 
-import java.util.Scanner;
-
 public class LogikaiOOP_csoport {
    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  
-        Lada a = new Lada();
-        
+        new Feladat(0);
     }
     
 }
