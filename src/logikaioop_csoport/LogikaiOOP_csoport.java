5a3f198037a4f43e8c2d75a2d01518b6f52b2ad1
Venc03 <vegi.dani1@gmail.com>
2024-01-08 13:26:56 +0100

    elso feltoltes

---
+++ src/logikaioop_csoport/LogikaiOOP_csoport.java
@@ -0,0 +1,12 @@
+package logikaioop_csoport;
+public class LogikaiOOP_csoport {
+    public static void main(String[] args) {
+        System.out.println("Leírás");
+        System.out.println("Az 1. állítás!");
+        System.out.println("A 2. állítás!");
+        System.out.println("A 3. állítás!");
+        System.out.println("Melyik:");
+        System.out.println("A válasz jo | rossz");
+    }
+    
+}
