535209fea3d62a9085a673f5e4e07c6305b53cf3
VégiDánielMárk(SZF_2 <VgiDnielMrk(SZF_2@Tanulo-230-18>
2024-01-10 12:57:51 +0100

    mergeles

--- src/logikaioop_csoport/LogikaiOOP_csoport.java
+++ src/logikaioop_csoport/LogikaiOOP_csoport.java
@@ -1,19 +1,10 @@
 package logikaioop_csoport;
 
 public class LogikaiOOP_csoport {
-<<<<<<< HEAD
 
     public static void main(String[] args) {
         new Feladat(0);
 
     }
-    
-}
-=======
->>>>>>> 23efb3bf4232fd3314a5527b88640d9b7a3c076a
 
-    public static void main(String[] args) {
-        new Adatbekeres().run();
-
-    }
 }
